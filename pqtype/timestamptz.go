cb06fb37dba60aec7144423199148757f0271aa2
felix <fe.info@roewell.de>
2021-07-24 11:55:14 +0200

    fix DecodeBinary for pgx

--- pqtype/timestamptz.go
+++ pqtype/timestamptz.go
@@ -21,8 +21,7 @@ const (
 )
 
 func (v *Timestamptz) FromBinary(src []byte) ([]byte, error) {
-	const size = valueOffset + timestamptzSize
-	if len(src) < size {
+	if len(src) < valueOffset + timestamptzSize {
 		return nil, ErrInsufficientBytes
 	}
 
@@ -31,7 +30,11 @@ func (v *Timestamptz) FromBinary(src []byte) ([]byte, error) {
 		return nil, &DecodeTypeErr{expected: TimestamptzOID, got: typ}
 	}
 
-	microsecSinceY2K := int64(binary.BigEndian.Uint64(src[valueOffset:]))
+	return v.fromBinary(src[valueOffset:])
+}
+
+func (v *Timestamptz) fromBinary(src []byte) ([]byte, error) {
+	microsecSinceY2K := int64(binary.BigEndian.Uint64(src))
 	switch microsecSinceY2K {
 	case inftyMicroSecOffset:
 		v.infty = 1
@@ -43,12 +46,15 @@ func (v *Timestamptz) FromBinary(src []byte) ([]byte, error) {
 		v.infty = 0
 	}
 
-	return src[size:], nil
+	return src[timestamptzSize:], nil
 }
 
 func (v *Timestamptz) DecodeBinary(_ *pgtype.ConnInfo, src[]byte) error {
-	var err error
-	_, err = v.FromBinary(src)
+	if len(src) < valueOffset + timestamptzSize {
+		return ErrInsufficientBytes
+	}
 
+	var err error
+	_, err = v.fromBinary(src)
 	return err
 }
