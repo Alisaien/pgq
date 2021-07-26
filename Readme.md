fcfca46215c2f937d11a5803d653c3bbad0f6005
felix <fe.info@roewell.de>
2021-07-26 14:03:23 +0200

    remove support for date type

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+## Date type
+Postgres Date type is intentionally not supported
+because it creates pitfalls with timezones, use timestamptz instead
---
+++ doc.go
@@ -0,0 +1,4 @@
+package pgq
+
+// Postgres Date type is intentionally not supported
+// because it creates pitfalls with timezones, use timestamptz instead
